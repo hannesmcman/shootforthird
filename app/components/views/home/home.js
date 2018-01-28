278f036f127e5c2cfe008008cb8fd399b6c20dd9
Cole Wallin <cole.wallin@gmail.com>
2018-01-28 13:32:37 -0600

    Buttons

--- app/components/views/home/home.js
+++ app/components/views/home/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {View, Text, Alert, AppRegistry, Button, StyleSheet, TouchableWithoutFeedback} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import ProgressCircle from 'react-native-progress-circle'
-import * as c from '../../colors.js'
+import * as c from '../../colors'
 
 
 
@@ -57,10 +57,21 @@ export default class HomeScreen extends React.Component {
             title="            "
           />
         </View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={styles.boxes}>
+          <View style={{width: 100, height: 100, backgroundColor: c.iceberg}}>
+            <Text style={styles.box}>Physician</Text>
+          </View>
+          <View style={{width: 100, height: 100, backgroundColor: c.platinum}}>
+            <Text style={styles.box}>Time</Text>
+          </View>
+          <View style={{width: 100, height: 100, backgroundColor: c.iceberg}}>
+            <Text style={styles.box}>Help</Text>
+          </View>
+        </View>
       </View>
+
+
+
 
     )
   }
@@ -72,6 +83,12 @@ const styles = StyleSheet.create({
    paddingTop: 30,
    // justifyContent: 'center',
    alignItems: 'center'
+  },
+  boxes: {
+   flex: 1,
+   // justifyContent: 'center',
+   flexDirection: 'row',
+   justifyContent: 'space-between',
   },
   buttonContainer: {
     margin: 20
@@ -92,5 +109,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontSize: 20,
     textAlign: 'center'
+  },
+  box: {
+    marginTop: 10,
+    marginBottom: 20,
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'darkcyan'
   },
 })
