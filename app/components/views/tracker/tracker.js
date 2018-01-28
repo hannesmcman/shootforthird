b96c84f22596844f43aa8ce18c747b8aeaf95d09
Cole Wallin <cole.wallin@gmail.com>
2018-01-27 23:52:57 -0600

    Calendar to the tracker

--- app/components/views/tracker/tracker.js
+++ app/components/views/tracker/tracker.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {View, Text} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Calendar, CalendarList } from 'react-native-calendars'; // 1.5.3
+
 
 export default class TrackerScreen extends React.Component {
   static navigationOptions = {
@@ -14,10 +16,32 @@ export default class TrackerScreen extends React.Component {
     ),
   };
 
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Tracker Screen</Text>
+      <View style={{ paddingTop: 50, flex: 1 }}>
+        <Calendar
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {console.log('selected day', day)}}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MMM yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {console.log('month changed', month)}}
+          // Hide month navigation arrows. Default = false
+          hideArrows={false}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+          // Hide day names. Default = false
+          hideDayNames={false}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={false}
+        />
       </View>
     );
   }
